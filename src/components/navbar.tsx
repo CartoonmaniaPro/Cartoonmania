1af3a9e2b4af950b477567d0b7c1f7549698decc
Jesus Mendoza <jesusmdz929@gmail.com>
2024-06-06 04:52:40 -0600

    fix 19

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,21 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href={import.meta.env.BASE_URL}>Cartoonmania</a>
-        <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+        <a
+          className="navbar-brand font-weight-bolder ms-lg-0 "
+          href={import.meta.env.BASE_URL}
+        >
+          Cartoonmania
+        </a>
+        <button
+          className="navbar-toggler shadow-none ms-2"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navigation"
+          aria-controls="navigation"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon mt-2">
             <span className="navbar-toggler-bar bar1"></span>
             <span className="navbar-toggler-bar bar2"></span>
@@ -12,17 +25,23 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navigation">
           <ul className="navbar-nav ms-auto">
-        
-         
-  
             <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="shopping-cart">
-              <i className="fa-solid text-lg fa-cart-shopping"></i>
+              <a
+                className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 "
+                aria-current="page"
+                href={import.meta.env.BASE_URL + "shopping-cart"}
+              >
+                <i className="fa-solid text-lg fa-cart-shopping"></i>
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page"  rel="nofollow" target="_blank">
-              <i className="fas text-lg fa-user"></i>
+              <a
+                className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 "
+                aria-current="page"
+                rel="nofollow"
+                target="_blank"
+              >
+                <i className="fas text-lg fa-user"></i>
               </a>
             </li>
           </ul>
